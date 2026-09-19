d6372bb2223d6cd411246a51b316811e2ed47a41
RafaelCaliman <rafaelrosacaliman123@gmail.com>
2026-09-19 13:21:04 -0300

    feat: configura banco de dados e conexão

---
+++ HotDogBrasilia/HotDogBrasilia.Data/ConexaoBanco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HotDogBrasilia.Data
+{
+    public class ConexaoBanco
+    {
+        private const string ConnectionString = "Server=localhost;Database=hotdog;User=root;Password=";
+
+        public static MySqlConnection ObterConexao()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+    }
+}
